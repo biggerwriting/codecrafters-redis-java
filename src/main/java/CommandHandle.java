9163b2bb79d7c5fdeeab861ca2c7606d1afd5c03
tongqianwen <tongqianwen-it@bestpay.com.cn>
2024-11-24 14:09:58 +0800

    持久化配置

--- src/main/java/CommandHandle.java
+++ src/main/java/CommandHandle.java
@@ -1,10 +1,8 @@
-package demo;
+import demo.ExpiryValue;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -12,7 +10,10 @@ import java.util.concurrent.ConcurrentHashMap;
  * @Date: 2024/11/23
  */
 public class CommandHandle extends Thread {
+    public static Map<String, String> config = new HashMap<String, String>();
     public static ConcurrentHashMap<String, ExpiryValue> setDict = new ConcurrentHashMap<>();
+    public static final String CONFIG_DBFILENAME="dbfilename";
+    public static final String CONFIG_DIR="dir";
 
     private final Socket socket;
 
@@ -42,14 +43,14 @@ public class CommandHandle extends Thread {
                     }
                     case "ECHO": {
                         String message = tokens.size() > 1 ? tokens.get(1) : "";
-                        response = String.format("$%d\r\n%s\r\n", message.length(), message);
+                        response = buildResponse(message);
                         break;
                     }
                     case "GET": {
                         ExpiryValue expiryValue = setDict.get(tokens.get(1));
                         if (expiryValue != null && expiryValue.expiry > System.currentTimeMillis()) {
                             String message = expiryValue.value;
-                            response = String.format("$%d\r\n%s\r\n", message.length(), message);
+                            response = buildResponse(message);
                         } else {
                             setDict.remove(tokens.get(1));
                             response = "$-1\r\n";
@@ -65,6 +66,24 @@ public class CommandHandle extends Thread {
                         response = "+OK\r\n";
                         break;
                     }
+                    case "CONFIG":{
+                        // the expected response to CONFIG GET dir is:
+                        //*2\r\n$3\r\ndir\r\n$16\r\n/tmp/redis-files\r\n
+                        if("get".equalsIgnoreCase(tokens.get(2))){
+                            if(CONFIG_DIR.equalsIgnoreCase(tokens.get(3))){
+                                String dir = config.get(CONFIG_DIR);
+                                response ="*2\r\n"+ buildResponse(CONFIG_DIR)+buildResponse(dir);
+                            }else if(CONFIG_DBFILENAME.equalsIgnoreCase(tokens.get(3))){
+                                String dbfilename = config.get(CONFIG_DBFILENAME);
+                                response ="*2\r\n"+ buildResponse(CONFIG_DBFILENAME)+buildResponse(dbfilename);
+                            }
+                        }else {
+
+                            response = "$-1\r\n";
+                        }
+                        break;
+
+                    }
                     default: {
                         response = "$-1\r\n";
                         break;
@@ -79,6 +98,12 @@ public class CommandHandle extends Thread {
             e.printStackTrace();
         }
 
+    }
+
+    private static String buildResponse(String message) {
+        String response;
+        response = String.format("$%d\r\n%s\r\n", message.length(), message);
+        return response;
     }
 
     List<String> parse(String param) {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
-import demo.CommandHandle;
-import demo.ExpiryValue;
-
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,6 +11,17 @@ public class Main {
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
+        for (int i = 0; i < args.length; i++) {
+            //./your_program.sh  --dir /tmp/redis-files --dbfilename dump.rdb
+            String param = args[i];
+            if ("--dir".equalsIgnoreCase(param)) {
+                CommandHandle.config.put(CommandHandle.CONFIG_DIR, args[++i]);
+            }
+            if ("--dbfilename".equalsIgnoreCase(param)) {
+                CommandHandle.config.put(CommandHandle.CONFIG_DBFILENAME, args[++i]);
+            }
+
+        }
 
         //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
