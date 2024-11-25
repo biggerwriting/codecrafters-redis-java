ab35fdaac01239445652201e3976fdccf50f3237
tongqianwen <tongqianwen-it@bestpay.com.cn>
2024-11-25 13:23:24 +0800

    send handshake ping

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
-import java.io.IOException;
+import demo.CommandHandle;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-import demo.CommandHandle;
 
 import static demo.CommandHandle.*;
 
@@ -71,11 +71,33 @@ public class Main {
                 String[] master = args[++i].split(" ");// <MASTER_HOST> <MASTER_PORT>
                 config.put(CommandHandle.MASTER_HOST, master[0]);
                 config.put(CommandHandle.MASTER_PORT, master[1]);
+                handShake();
             }
         }
         // master_replid and master_repl_offset
-        config.put(MASTER_REPLID,"8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb");
-        config.put(MASTER_REPL_OFFSET,"0");
+        config.put(MASTER_REPLID, "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb");
+        config.put(MASTER_REPL_OFFSET, "0");
+    }
+
+    private static void handShake() {
+        String host = config.get(MASTER_HOST);
+        int port = Integer.parseInt(config.get(MASTER_PORT));
+
+        try (Socket socket = new Socket(host, port); InputStream inputStream = socket.getInputStream(); InputStreamReader inputStreamReader = new InputStreamReader(inputStream); BufferedReader bufferedReader = new BufferedReader(inputStreamReader); OutputStream outputStream = socket.getOutputStream();) {
+            outputStream.write("*1\r\n$4\r\nPING\r\n".getBytes());
+            System.out.println("得到服务端输出：【" + readBuffLine(inputStreamReader) + "】");
+
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static String readBuffLine(InputStreamReader inputStreamReader) throws IOException {
+        char[] charArray = new char[1024];
+        int readLength = inputStreamReader.read(charArray);
+        String line = new String(charArray, 0, readLength);
+        return line;
     }
 
     private static int getPort() {
